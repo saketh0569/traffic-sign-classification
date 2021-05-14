5084102daffc40b8387e8a60060d5ef62ad528fc
saketh <mkrishnasaketh2001@gmail.com>
2021-05-14 23:06:24 +0530

    input from camera

--- minor_project_classification_of_traffic_signs.py
+++ minor_project_classification_of_traffic_signs.py
@@ -760,7 +760,76 @@ plt.show()
 ## Step 6: Testing the Model on New Images
 
 In this step, we will use the model to predict traffic signs type of 5 random images of German traffic signs from the web our model's performance on these images.
+
+Now, we will take input from the Camera
 """
+
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
+
+def take_photo(filename='photo.jpg', quality=0.8):
+  js = Javascript('''
+    async function takePhoto(quality) {
+      const div = document.createElement('div');
+      const capture = document.createElement('button');
+      capture.textContent = 'Capture';
+      div.appendChild(capture);
+
+      const video = document.createElement('video');
+      video.style.display = 'block';
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+      document.body.appendChild(div);
+      div.appendChild(video);
+      video.srcObject = stream;
+      await video.play();
+
+      // Resize the output to fit the video element.
+      google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+      // Wait for Capture to be clicked.
+      await new Promise((resolve) => capture.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+      div.remove();
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+    ''')
+  display(js)
+  data = eval_js('takePhoto({})'.format(quality))
+  binary = b64decode(data.split(',')[1])
+  with open(filename, 'wb') as f:
+    f.write(binary)
+  return filename
+
+import os
+path = "/content/new_test_images"
+os.mkdir(path)
+
+from IPython.display import Image
+import os
+n = 3
+try:
+  for i in range(n):
+    filename = take_photo('/content/new_test_images/photo.jpg')
+    print('Saved to {}'.format(filename))
+  
+    # Show the image which was just taken.
+    display(Image(filename))
+    # rename the file
+    os.rename('/content/new_test_images/photo.jpg', '/content/new_test_images/photo' + str(i+1) + '.jpg')
+    
+except Exception as err:
+  # Errors will be thrown if the user does not have a webcam or if they do not
+  # grant the page permission to access it.
+  print(str(err))
+
+"""End of taking input from Camera"""
 
 # Loading and resizing new test images
 new_test_images = []
